12206c5deb8d69cb82257842671345c476a788e9
KohTongLiang <koh.tongliang@gmail.com>
2020-09-17 16:28:22 +0800

    Small. Added the genome file and prompt user input.

--- CZ2001 Lab Project 1/src/Main.java
+++ CZ2001 Lab Project 1/src/Main.java
@@ -1,24 +1,32 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String textStr = "ASDASDBKASJDBKSAJDBASBJDSDASDA";
-		String patternStr = "KAS";
+		String textStr = "";
+		// example search: TCAGTGGAGGAAGCG
+		 
+        try {
+        	textStr = new String ( Files.readAllBytes( Paths.get("genome.fna") ) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Please enter the pattern you wish to search: ");
+        Scanner sc = new Scanner(System.in);
+        String patternStr = sc.nextLine();
+		
 		char[] text = textStr.toCharArray();
 		char[] pattern = patternStr.toCharArray();
 		
 		System.out.println("The text is : " + textStr);
 		System.out.println("The pattern is : " + patternStr);
 		
-		int position = compare(text, pattern);
-		
-		if (position == -1) {
-			System.out.println("No Match Found");
-		} else {
-			System.out.println("Found at " + position);
-		}
-	}
+		compare(text, pattern);
+	} // end of Main method
 	
 	static int compare (char[] text, char[] pattern) {
 		if (pattern.length == 0) {
@@ -27,43 +35,45 @@ public class Main {
 		
         int charTable[] = makeCharTable(pattern);
         int offsetTable[] = makeOffsetTable(pattern);
+        int i = pattern.length - 1;
+        int j;
         
-        for (int i = pattern.length - 1, j; i < text.length;) {
-            for (j = pattern.length - 1; pattern[j] == text[i]; --i, --j)  {
+        while (i < text.length) {
+            for (j = pattern.length - 1; pattern[j] == text[i]; i--, j--)  {
             	if (j == 0) {
-                    return i;
+        			System.out.println("Found at poisition " + i);
+        			break;
             	}
-            }
+            } // end of comparison at position i.
             
+            // compare which rule (bad character heuristics or good suffix) gives a greater jump
             i += Math.max(offsetTable[pattern.length - 1 - j], charTable[text[i]]);
-        }
+        } // end of while loop
         
         return -1;
 	} // end of compare method
 	
 	
-	///// Boyer Moore Methods
 	static int[] makeCharTable(char[] pattern) {
-       final int SIZE = 256;
+       int[] table = new int[256];
        
-       int[] table = new int[SIZE];
-       
-       for (int i = 0; i < table.length; ++i) {
+       for (int i = 0; i < table.length; i++) {
     	   table[i] = pattern.length;
        }
        
-       for (int i = 0; i < pattern.length - 1; ++i) {
+       for (int i = 0; i < pattern.length - 1; i++) {
     	   table[pattern[i]] = pattern.length - 1 - i;
        }
+       
        return table;
-     } // 
+   } // 
 	 
-     /** Makes the jump table based on the scan offset which mismatch occurs. **/
-     private static int[] makeOffsetTable(char[] pattern) {
+   /** Makes the jump table based on the scan offset which mismatch occurs. **/
+   private static int[] makeOffsetTable(char[] pattern) {
        int[] table = new int[pattern.length];
        int lastPrefixPosition = pattern.length;
        
-       for (int i = pattern.length - 1; i >= 0; --i) {
+       for (int i = pattern.length - 1; i >= 0; i--) {
            if (isPrefix(pattern, i + 1)) { 
         	   lastPrefixPosition = i + 1;
            }
@@ -71,7 +81,7 @@ public class Main {
            table[pattern.length - 1 - i] = lastPrefixPosition - i + pattern.length - 1;
        }
        
-       for (int i = 0; i < pattern.length - 1; ++i) {
+       for (int i = 0; i < pattern.length - 1; i++) {
              int slen = suffixLength(pattern, i);
              table[slen] = pattern.length - 1 - i + slen;
        }
@@ -81,7 +91,7 @@ public class Main {
      
    /** function to check if needle[p:end] a prefix of pattern **/
    private static boolean isPrefix(char[] pattern, int p) {
-       for (int i = p, j = 0; i < pattern.length; ++i, ++j) {
+       for (int i = p, j = 0; i < pattern.length; i++, j++) {
     	   if (pattern[i] != pattern[j]) {
                  return false;
     	   }
@@ -93,10 +103,9 @@ public class Main {
    /** function to returns the maximum length of the substring ends at p and is a suffix **/
    private static int suffixLength(char[] pattern, int p) {
        int len = 0;
-       for (int i = p, j = pattern.length - 1; i >= 0 && pattern[i] == pattern[j]; --i, --j) {
+       for (int i = p, j = pattern.length - 1; i >= 0 && pattern[i] == pattern[j]; i--, j--) {
     	   len += 1;
        }
-       
        return len;
    }
 
